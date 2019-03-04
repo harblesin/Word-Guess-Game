f80e8f62bb9e4fa43c6a10f86d6eda99c9f75f91
ylech <waltermelonson@gmail.com>
2019-03-04 11:12:34 -0700

    laptop progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,27 @@
 
-wordsGroup = ("sand", "tomato");
+wordsGroup =["sand", "tomato"];
 
 
     var pick = wordsGroup[Math.floor(Math.random() * wordsGroup.length)];
-
-    
-    for(var i = 0; i < 10; i++){
-
-
     console.log(pick);
-    var mystWord = possible.split(" ");
-    console.log(mystWord);
-    }
+    guessLine = [];
+    placeholderLine = [];
     
+    for(var i = 0; i < pick.length; i++){
+    var mystWord = pick.split("");
+    guessLine.push(mystWord[i]);
+    }
+    for( var j = 0; j < guessLine.length; j ++){
+                    placeholderLine.push("_ ");
 
+    
+    }
+        var placeholderLineText = document.getElementsByClassName("box").appendParent += placeholderLine;
+
+console.log(mystWord);    
+console.log(placeholderLine);
+console.log(guessLine);
+ 
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
